50647fedcf4041089824abc4cd7b4e65b23e4ed1
Doug Elkin <1406429+duhdugg@users.noreply.github.com>
2026-04-06 17:21:01 -0400

    update step submodule (fdb1680c21f4..bc740f568a5e)
    1f40b0224 Fix copy in interlinear mode
    4e435f8fc Update version number
    eaa49900b Sidebar definition adjustment and alt morphologies, small genealogy-chronology changes (#344)
    746139379 Grant Horner, minor fixes (welcome close button, checkbox alignment, opendyslexic locally) (#359)
    b7a42b16c Dark mode style fixes (#361) (#362)
    cb21dcfcc Fix to show morphology when first Bible has none
    bc740f568 Merge remote-tracking branch 'upstream/master' into duhdugg-master

--- oci/step-build/deploy.sh
+++ oci/step-build/deploy.sh
@@ -7,4 +7,7 @@ cp /step-build/step-server-*.jar /opt/step/
 
 # FIXME dynamic version
 rm /opt/step/step-server-26.1.2.jar
-sed --in-place 's/26.1.2/26.3.4/g' /opt/step/step-install4j
+# if/when other things in step are modified, old jars need removed
+# rm /opt/step/step-web/WEB-INF/lib/step-core-26.1.2.jar
+
+sed --in-place 's/26.1.2/26.3.9/g' /opt/step/step-install4j
